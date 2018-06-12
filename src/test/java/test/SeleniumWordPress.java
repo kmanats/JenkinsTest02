f713d820469259ad42d29aff3b010e73a1aa08a3
Kati <Kati@10.201.40.227>
2018-06-12 12:24:25 +0200

    Word Press Contributers

--- src/test/java/test/SeleniumWordPress.java
+++ src/test/java/test/SeleniumWordPress.java
@@ -24,7 +24,7 @@ public class SeleniumWordPress {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         baseUrl = "http://localhost/";
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
